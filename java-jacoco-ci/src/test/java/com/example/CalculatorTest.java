a62e2a3337e416ed7ae1ae643d8a1e68ff996f26
nisa özkanca <nozkanca7@gmail.com>
2025-05-15 01:13:18 +0300

    Add files via upload

---
+++ java-jacoco-ci/src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    @Test
+    void testAdd() {
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(2, 3));
+    }
+}
